7f4c1a009291d97d67bbcafeff3c8f54cfb00b93
yin-zt <15626499421@163.com>
2022-11-12 22:49:47 +0800

    first commit push with simple framework

--- firsthouseui/src/components/login/Login.vue
+++ firsthouseui/src/components/login/Login.vue
@@ -1,9 +1,80 @@
 <template>
-    登录页面
+    <div id="container">
+        <div id="title">
+            <h1>DNS域名记录管理系统</h1>
+        </div>
+        <div class="input">
+            账号：<el-input v-model="name" prefix-icon="el-icon-user" placeholder="请输入用户名"></el-input>
+        </div>
+        <div class="input">
+            密码：<el-input v-model="password" prefix-icon="el-icon-lock" placeholder="请输入密码" auto-complete="new-password" show-password></el-input>
+        </div>
+        <div class="input">
+            <el-button @click="login" style="width:500px" type="primary" :disabled="disabled">登录</el-button>
+        </div>
+    </div>
 </template>
 
 <script>
-    export default{
-        name: "Login"
+import Storage from '../../tools/Storage'
+import { ElMessage } from 'element-plus';
+// import { thisExpression } from '@babel/types';
+
+export default {
+    name:"Login",
+    data() {
+        return {
+            name:"",
+            password:""
+        }
+    },
+    // mounted () {
+    //     this.name = ""
+    //     this.password = ""
+    // },
+    computed: {
+        disabled(){
+            return this.name.length == 0 || this.password.length == 0;
+        }
+    },
+    methods: {
+        login() {
+            Storage.commit("registUserInfo",{
+                name:this.name,
+                password:this.password
+            })
+            ElMessage({
+                message:'登录成功', 
+                type:'success', 
+                duration:1500
+            })
+            setTimeout(() => {
+                this.$router.push({name:"home"})
+            }, 3000);
+        }
     }
+}
 </script>
+
+<style scoped>
+#container {
+    background: #595959;
+    background-image: url("~@/assets/login_bg.jpg");
+    height: 100%;
+    width: 100%;
+    position: absolute;
+}
+
+#title {
+    text-align: center;
+    color: azure;
+    margin-top: 200px;
+}
+
+.input {
+    margin: 20px auto;
+    width: 500px;
+}
+
+
+</style>
--- firsthouseui/src/tools/Storage.js
+++ firsthouseui/src/tools/Storage.js
@@ -12,16 +12,16 @@ const Store = createStore({
             return state.userName.length > 0
         }
     },
-    // mutations: {
-    //     clearUserInfo(state) {
-    //         state.userName = "";
-    //         state.userPassword = "";
-    //     },
-    //     registUserInfo(state,{name, password}) {
-    //         state.userName = name;
-    //         state.password = password;
-    //     }
-    // }
+    mutations: {
+        clearUserInfo(state) {
+            state.userName = "";
+            state.userPassword = "";
+        },
+        registUserInfo(state,{name, password}) {
+            state.userName = name;
+            state.password = password;
+        }
+    }
 })
 
 export default Store;
